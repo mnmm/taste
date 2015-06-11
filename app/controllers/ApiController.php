b45d0d82fc18bde8bdefad2a8ed6b64ed6195a02
weeckie <weeckie@gmail.com>
2015-06-11 15:47:27 +0530

    paymentdone hover

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -1623,7 +1623,9 @@ class ApiController extends BaseController {
 								}
 							}
 							//$requestinfohtml = PoDetail::get_request_info($listingdetail->vendor_id);
-							$result['data'][]= array($listingdetail->po_no,$podate,$listingdetail->vendor_name,$shipping_address,'$'.$listingdetail->total_amount,$duedate,$paid_status_html,$priority_status,'<a '.$payment_done_hover.' href="'.$payhtml.'" class="btn btn-xs default btn-editable '.$pay_done_class.'" id="'.$listingdetail->po_no.'" data-payment-amount="'.$listingdetail->total_amount.'" data-placement="top">Pay</a><button class="btn btn-xs  default btn-editable requestinfo" id="'.$request_id.'" data-request-id="'.$listingdetail->vendor_id.'">'.$request_status.'</button>');
+							$result['data'][]= array($listingdetail->po_no,$podate,$listingdetail->vendor_name,$shipping_address,'$'.$listingdetail->total_amount,$duedate,$paid_status_html,$priority_status,'<script type="text/ng-template" id="routingpopover.html">
+							Your routing number is normally found on a check provided by your bank.
+							</script><a '.$payment_done_hover.' href="'.$payhtml.'" class="btn btn-xs default btn-editable '.$pay_done_class.'" id="'.$listingdetail->po_no.'" data-payment-amount="'.$listingdetail->total_amount.'" data-placement="top">Pay</a><button class="btn btn-xs  default btn-editable requestinfo" id="'.$request_id.'" data-request-id="'.$listingdetail->vendor_id.'">'.$request_status.'</button>');
 						//}
 						$i++;
 					}
