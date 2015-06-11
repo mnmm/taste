2985fe334a4dc9040e476244e2718a897bc53d75
weeckie <weeckie@gmail.com>
2015-06-11 13:08:41 +0530

    Add Bank Info

--- js/controllers/VendorBankAccountController.js
+++ js/controllers/VendorBankAccountController.js
@@ -342,6 +342,7 @@ MetronicApp.controller('VendorBankAccountController', function($rootScope, $scop
 	}
 	
 	$scope.openBankAccountPopUp= function () {
+			console.log('comes here');
 			var vendoruserid  = localStorage.getItem('userid');
 			$http.defaults.headers.common['x-taste-request-timestamp'] = Math.floor((new Date().getTime()/1000));
 			$http.defaults.headers.common['x-taste-access-token'] =localStorage.getItem('access_token');
