87ba01a329c0f303f883761bd1961c644fdd1898
weeckie <weeckie@gmail.com>
2015-06-12 16:46:53 +0530

    Payment page

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -1673,7 +1673,11 @@ class ApiController extends BaseController {
 									$transfer_date = date("n/j g:ia", $transferdata['data']->date);
 									
 									$transfer_amount = ($transferdata['data']->amount/100);
-									$get_transfer_user_name = PoDetail::get_user_detail_for_transfer($listingdetail->payment_done_by);
+									if(isset($listingdetail->payment_done_by) && $listingdetail->payment_done_by != ''){
+										$get_transfer_user_name = PoDetail::get_user_detail_for_transfer($listingdetail->payment_done_by);
+									} else {
+										$get_transfer_user_name = '';
+									}
 									
 									$payment_title = 'data-toggle="popover" title="Payment Details"';
 									$payment_content = 'Name : '.$get_transfer_user_name.'<br/>Payment date : '.$transfer_date.'<br/>Amuont : $'.$transfer_amount;
