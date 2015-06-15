5f2568fc4667fd8afb13e79456b4b579d7b7af6d
weeckie <ashishjain@outlook.com>
2015-06-15 16:28:45 +0530

    Authorization roll

--- js/controllers/LoginController.js
+++ js/controllers/LoginController.js
@@ -4,7 +4,7 @@
 
 MetronicApp.controller('LoginController', function($rootScope, $scope, $http, $timeout, $location,$window ,authenticationSvc) {
 	$scope.apppath= 'https://mnmdesignlabs.com/taste';
-	$scope.login = function(){authenticationSvc.login($scope.email,$scope.password,'dashboard.html');}
+	$scope.login = function(){authenticationSvc.login($scope.email,$scope.password,'vendors');}
 	 
     $rootScope.settings.layout.pageBodySolid = true;
     $rootScope.settings.layout.pageSidebarClosed = true;
