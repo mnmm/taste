8acd17408694a48d3b7c094fb04ff12122522f85
weeckie <weeckie@gmail.com>
2015-06-17 10:26:05 +0530

    Login page

--- app/controllers/AdminDashboardController.php
+++ app/controllers/AdminDashboardController.php
@@ -18,7 +18,7 @@ class AdminDashboardController extends BaseController {
 	public function viewdashboard() {
 		$uri = Request::path();
 		echo $uri;exit;
-		//return View::make('index');
+		return View::make('index');
 	}
 
 }
