f71fd6c2041798759c019dcfd30e22e3859d8da5
weeckie <weeckie@gmail.com>
2015-06-17 11:47:47 +0530

    Login page

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -121,6 +121,7 @@ License: You must have a valid license purchased only from themeforest(the above
 	<script src="../../../assets/global/plugins/respond.min.js"></script>
 	<script src="../../../assets/global/plugins/excanvas.min.js"></script> 
 	<![endif]-->
+	<div id="" ng-if="showHeader">
 	<script src="<?php echo Request::root(); ?>/assets/global/plugins/jquery.min.js" type="text/javascript"></script>
 	<script src="<?php echo Request::root(); ?>/assets/global/plugins/jquery-migrate.min.js" type="text/javascript"></script>
 	<script src="<?php echo Request::root(); ?>/assets/global/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
@@ -161,7 +162,22 @@ License: You must have a valid license purchased only from themeforest(the above
 			Metronic.setAssetsPath('http://localhost/laravel/assets/'); // Set the assets folder path			
 		});
 	</script>
-	<!-- END JAVASCRIPTS -->
+	</div>
+	<script src="<?php echo Request::root(); ?>/assets/global/plugins/jquery.min.js" type="text/javascript"></script>
+	<script src="<?php echo Request::root(); ?>/assets/global/plugins/jquery-migrate.min.js" type="text/javascript"></script>
+	<script src="<?php echo Request::root(); ?>/assets/global/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="<?php echo Request::root(); ?>/assets/global/plugins/jquery.blockui.min.js" type="text/javascript"></script>
+	<script src="<?php echo Request::root(); ?>/assets/global/plugins/jquery.cokie.min.js" type="text/javascript"></script>
+	<script src="<?php echo Request::root(); ?>/assets/global/plugins/uniform/jquery.uniform.min.js" type="text/javascript"></script>
+	<!-- END CORE PLUGINS -->
+	<!-- BEGIN PAGE LEVEL PLUGINS -->
+	<script src="<?php echo Request::root(); ?>/assets/global/plugins/jquery-validation/js/jquery.validate.min.js" type="text/javascript"></script>
+	<!-- END PAGE LEVEL PLUGINS -->
+	<!-- BEGIN PAGE LEVEL SCRIPTS -->
+	<script src="<?php echo Request::root(); ?>/assets/global/scripts/metronic.js" type="text/javascript"></script>
+	<script src="<?php echo Request::root(); ?>/assets/admin/layout/scripts/layout.js" type="text/javascript"></script>
+	<script src="<?php echo Request::root(); ?>/assets/admin/layout/scripts/demo.js" type="text/javascript"></script>
+		<!-- END JAVASCRIPTS -->
 </body>
 <!-- END BODY -->
 </html>
