56515ae959005407020bee3ffe11c97f5d9d1d8e
weeckie <weeckie@gmail.com>
2015-06-17 10:49:28 +0530

    Login page

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -164,4 +164,51 @@ License: You must have a valid license purchased only from themeforest(the above
 	<!-- END JAVASCRIPTS -->
 </body>
 <!-- END BODY -->
+<body class="login" ng-controller="AppController" class="page-header-fixed page-sidebar-closed-hide-logo page-quick-sidebar-over-content page-on-load" ng-class="{'page-container-bg-solid': settings.layout.pageBodySolid, 'page-sidebar-closed': settings.layout.pageSidebarClosed}">
+<!-- BEGIN SIDEBAR TOGGLER BUTTON -->
+<div class="menu-toggler sidebar-toggler">
+</div>
+<!-- END SIDEBAR TOGGLER BUTTON -->
+<!-- BEGIN LOGO -->
+<div class="logo">
+	<a href="index.html">
+	<img src="../../assets/admin/layout/img/logo-big.png" alt=""/>
+	</a>
+</div>
+<div class="copyright">
+	 2014 © Metronic. Admin Dashboard Template.
+</div>
+<!-- END LOGIN -->
+<!-- BEGIN JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
+<!-- BEGIN CORE PLUGINS -->
+<!--[if lt IE 9]>
+<script src="../../assets/global/plugins/respond.min.js"></script>
+<script src="../../assets/global/plugins/excanvas.min.js"></script> 
+<![endif]-->
+<script src="<?php echo Request::root(); ?>/assets/global/plugins/jquery.min.js" type="text/javascript"></script>
+<script src="<?php echo Request::root(); ?>/assets/global/plugins/jquery-migrate.min.js" type="text/javascript"></script>
+<script src="<?php echo Request::root(); ?>/assets/global/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="<?php echo Request::root(); ?>/assets/global/plugins/jquery.blockui.min.js" type="text/javascript"></script>
+<script src="<?php echo Request::root(); ?>/assets/global/plugins/jquery.cokie.min.js" type="text/javascript"></script>
+<script src="<?php echo Request::root(); ?>/assets/global/plugins/uniform/jquery.uniform.min.js" type="text/javascript"></script>
+<!-- END CORE PLUGINS -->
+<!-- BEGIN PAGE LEVEL PLUGINS -->
+<script src="<?php echo Request::root(); ?>/assets/global/plugins/jquery-validation/js/jquery.validate.min.js" type="text/javascript"></script>
+<!-- END PAGE LEVEL PLUGINS -->
+<!-- BEGIN PAGE LEVEL SCRIPTS -->
+<script src="<?php echo Request::root(); ?>/assets/global/scripts/metronic.js" type="text/javascript"></script>
+<script src="<?php echo Request::root(); ?>/assets/admin/layout/scripts/layout.js" type="text/javascript"></script>
+<script src="<?php echo Request::root(); ?>/assets/admin/layout/scripts/demo.js" type="text/javascript"></script>
+<script src="<?php echo Request::root(); ?>/assets/admin/pages/scripts/login.js" type="text/javascript"></script>
+<!-- END PAGE LEVEL SCRIPTS -->
+<script>
+jQuery(document).ready(function() {     
+Metronic.init(); // init metronic core components
+Layout.init(); // init current layout
+Login.init();
+Demo.init();
+});
+</script>
+<!-- END JAVASCRIPTS -->
+</body>
 </html>
