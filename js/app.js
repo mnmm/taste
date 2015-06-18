689218072644b76bc2e3f7e908ebda3c80a34f1d
weeckie <weeckie@gmail.com>
2015-06-18 11:00:40 +0530

    Login screen

--- js/app.js
+++ js/app.js
@@ -1171,7 +1171,7 @@ MetronicApp.config(['$stateProvider', '$urlRouterProvider','USER_ROLES', functio
                               Globals.url+'/assets/global/plugins/jquery-validation/js/jquery.validate.min.js',
                               Globals.url+'/assets/global/plugins/jquery-validation/js/additional-methods.js',
                               Globals.url+'/assets/global/plugins/sumoselect/sumoselect.css',
-                             // Globals.url+'/assets/global/plugins/sumoselect/jquery.sumoselect.js',
+                              Globals.url+'/assets/global/plugins/sumoselect/jquery.sumoselect.js',
                               Globals.url+'/js/controllers/LoginController.js'
                         ]                    
                     });
