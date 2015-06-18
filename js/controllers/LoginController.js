f4d0ea388764313ef5f31f71bddb07530e456987
weeckie <weeckie@gmail.com>
2015-06-18 12:01:35 +0530

    Login screen

--- js/controllers/LoginController.js
+++ js/controllers/LoginController.js
@@ -233,7 +233,7 @@ MetronicApp.controller('LoginController', function($rootScope, $scope, $http, $t
 	$scope.blisterPackTemplates=[{id:'restaurant',name:"Restaurant"},{id:'caterer',name:"Caterer"},{id:'foodtruck',name:"Food Truck"},{id:'bakery',name:"Bakery"},{id:'other',name:"Other"}]
 	
 	$scope.changedValue=function(item){
-		console.log(item.name);
+		console.log(item);
     }     
 
 	if(authtoken != '' && typeof authtoken !== 'undefined' && authtoken !==null){
