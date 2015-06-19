825a9b6eb972077252fd83f4176b13341f81f1c8
weeckie <weeckie@gmail.com>
2015-06-19 17:18:50 +0530

    Admin Notes

--- app/models/AdminNotes.php
+++ app/models/AdminNotes.php
@@ -5,22 +5,6 @@ class AdminNotes extends Eloquent{
 	public $table = 'admin_notes';
 	public $timestamps = false;
 
-	public static function save_log($data) 
-	{
-		return $id = DB::table('admin_logs')->insertdelayed($data);
-	}
-
-
-	public static function sendadminnotes($sub,$message ,$user_id,$type='')
-    {
-       $created=date('Y-m-d H:i:s');
-	   $adminnoteid = DB::table('admin_notes')->insertGetId(
-    			array('subject' => $sub, 'message' => $message, 'user_id' => $user_id, 'created' => $created,'type' => $type)
-			);
-	  return $adminnoteid;
-	   
-    }
-    
     public function adminnotes()
 	{
 		$admin_notes_all= DB::table('admin_notes')
