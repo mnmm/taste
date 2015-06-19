e7f932179b3ae51bcde50f5d04ef9a8306a91150
weeckie <weeckie@gmail.com>
2015-06-19 16:32:46 +0530

    Admin Notes

--- js/controllers/LoginController.js
+++ js/controllers/LoginController.js
@@ -239,11 +239,11 @@ MetronicApp.controller('LoginController', function($rootScope, $scope, $http, $t
 			opendays = daysArr.join();
 		}
 		//console.log('opendays'+opendays);
-		console.log('opening'+$('input#businessopeninghour').val());
-		console.log('closing'+$('input#businessclosinghours').val());
-		/*$http.defaults.headers.common['x-taste-request-timestamp'] = Math.floor((new Date().getTime()/1000));
+		//console.log('opening'+$('input#businessopeninghour').val());
+		//console.log('closing'+$('input#businessclosinghour').val());
+		$http.defaults.headers.common['x-taste-request-timestamp'] = Math.floor((new Date().getTime()/1000));
 		$http.defaults.headers.common['x-taste-access-token'] =authtoken;
-		$http.post($scope.apppath+'/api/getunpaidpo',{fullname:$scope.fullname,email_address:$scope.email_address,password:$scope.register_password,address:$scope.address,city:$scope.city,state:$scope.state,zip:$scope.zip,phone:$scope.phone,location:$scope.location,address2:$scope.address2,crossstreet:$scope.crossstreet,neighborhood:$scope.neighborhood,entries:$scope.entries,daysopen:opendays,businessopeninghours:$scope.businessopeninghours,businessclosinghours:$scope.businessclosinghours,locationdescription:$scope.locationdescription,locationtype:locationtype,locationnotes:$scope.locationnotes,mealnotes:$scope.mealnotes,emailcontact:$scope.emailcontact,contactphone:$scope.contactphone,action:'createvendoraccount'}).
+		$http.post($scope.apppath+'/api/getunpaidpo',{fullname:$scope.fullname,email_address:$scope.email_address,password:$scope.register_password,address:$scope.address,city:$scope.city,state:$scope.state,zip:$scope.zip,phone:$scope.phone,location:$scope.location,address2:$scope.address2,crossstreet:$scope.crossstreet,neighborhood:$scope.neighborhood,entries:$scope.entries,daysopen:opendays,businessopeninghours:$('input#businessopeninghour').val(),businessclosinghours:$('input#businessclosinghour').val(),locationdescription:$scope.locationdescription,locationtype:locationtype,locationnotes:$scope.locationnotes,mealnotes:$scope.mealnotes,emailcontact:$scope.emailcontact,contactphone:$scope.contactphone,action:'createvendoraccount'}).
 			success(function(data) {
 				if(data.status_code === 200){
 					var subject = 'registered as new vendor';
@@ -269,7 +269,7 @@ MetronicApp.controller('LoginController', function($rootScope, $scope, $http, $t
 						} 
 					}
 				}
-		});*/
+		});
 	}
 	
 	function createauthtoken(){
