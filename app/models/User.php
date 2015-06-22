a71891883c1b4d39bcaa712403b9e74150049e04
weeckie <weeckie@gmail.com>
2015-06-22 11:02:16 +0530

    Invite vendors

--- app/models/User.php
+++ app/models/User.php
@@ -29,7 +29,7 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 		return $get_users;
 	}
 	
-	public function get_all_vendors(){
+	public static function get_all_vendors(){
 		$sortby=",invite_status asc,first_name";
 		$order="asc";
 		$result = DB::select( DB::raw("SELECT users.*, user_invite.status as invite_status, user_invite.created_date as sent_date from users LEFT JOIN user_invite on users.email = user_invite.email where users.usertype = 2 order by users.status asc $sortby $order"));   
