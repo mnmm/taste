828ad98ccc58e33daca5a70c2f310894946896c5
weeckie <weeckie@gmail.com>
2015-06-22 12:51:13 +0530

    Invite vendors

--- app/models/User.php
+++ app/models/User.php
@@ -68,7 +68,7 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 				$user_id  = Auth::user()->id; 
 				$user_details = User::get_user_complete_details($user_id);
 				if(count($user_details) > 0){
-					$username = $user_details[0]->first_name. ' '.$user_details[0]->last_name;
+					$username = $user_details[0]->fullname;
 				}
 				
 				$sub = 'created a new vendor';
