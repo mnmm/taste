89b175fdd0ee888eeeed007b95be45fff5e154cc
weeckie <weeckie@gmail.com>
2015-06-22 14:06:14 +0530

    Invite vendors

--- js/controllers/InviteController.js
+++ js/controllers/InviteController.js
@@ -573,7 +573,7 @@ MetronicApp.controller('InviteController', function($rootScope, $scope, $http, $
 							
 						})
 						.modal('show');
-						$('inviteVendor').find('textarea#message').wysihtml5({
+						$('inviteVendor').find('textarea.message').wysihtml5({
 								"stylesheets": ["https://mnmdesignlabs.com/taste/assets/global/plugins/bootstrap-wysihtml5/wysiwyg-color.css"]
 						});
 
