e393ec245826fb4ca3f10144fccce38f54099068
weeckie <weeckie@gmail.com>
2015-06-22 12:45:33 +0530

    Invite vendors

--- app/models/User.php
+++ app/models/User.php
@@ -66,7 +66,7 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 				
 				DB::table('user_invite')->insert(array( 'email' => $createAccountArr['email_address'],'status' => 2,'created_date'=>date('Y-m-d H:i:s'))); 
 				$user_id  = Auth::user()->id; 
-				$user_details = $this->get_user_complete_details($user_id);
+				$user_details = User::get_user_complete_details($user_id);
 				if(count($user_details) > 0){
 					$username = $user_details[0]->first_name. ' '.$user_details[0]->last_name;
 				}
