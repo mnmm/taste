e2df41606f92e06f60e52869d484b7798ea4fe21
weeckie <weeckie@gmail.com>
2015-06-23 18:48:57 +0530

    Change priority

--- app/models/AdminNotes.php
+++ app/models/AdminNotes.php
@@ -9,7 +9,7 @@ class AdminNotes extends Eloquent{
 	{
 		$admin_notes_all= DB::table('admin_notes')
 					->leftjoin('users','admin_notes.user_id', '=', 'users.id')
-					->select(DB::raw('admin_notes.id, admin_notes.subject, admin_notes.message, admin_notes.user_id, admin_notes.created, admin_notes.isread, admin_notes.status, admin_notes.type, date(admin_notes.created) as newdate ,users.name as username, users.email as useremail'))
+					->select(DB::raw('admin_notes.id, admin_notes.subject, admin_notes.message, admin_notes.user_id, admin_notes.created, admin_notes.isread, admin_notes.status, admin_notes.type, date(admin_notes.created) as newdate ,users.firstname as firstname, users.lastname as lastname,users.email as useremail'))
 					->orderBy('created', 'desc')
 					->get();
 		
